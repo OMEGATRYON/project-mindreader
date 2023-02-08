32db2818be0feee816028c16ebcb2faaed113d2a
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-02-08 19:16:37 +0000

    added javascript structure

---
+++ js/main.js
@@ -0,0 +1,75 @@
+let state = {
+    symbols: ['!', '@', '#', '$', '%', '^', '*', '&'],
+    page: [
+        {
+            headerText: "I can read your mind",
+            paragraphText: "",
+            nextButtonText: "",
+            page: 1,
+        },
+        {
+            headerText: "Pick a number from 0 - 99",
+            paragraphText: "When you have your number hit next",
+            nextButtonText: "NEXT",
+            page: 2,
+        },
+        {
+            headerText: "Add both digits together to get a new number",
+            paragraphText: "EX: 14 is 1 + 4 = 5, click next to proceed",
+            nextButtonText: "NEXT",
+            page: 3,
+        },
+        {
+            headerText: "Subtract your new number from the original number",
+            paragraphText: "EX: 14 - 5 = 9, Click next to proceed",
+            nextButtonText: "NEXT",
+            page: 4,
+        },
+        {
+            headerText: "ARRAY",
+            paragraphText: "Find your new number above by noting the symbol to the right of it.",
+            nextButtonText: "REVEAL",
+            page: 5,
+        },
+        {
+            headerText: "&",
+            paragraphText: "Your symbol is: &",
+            nextButtonText: "",
+            page: 6,
+        },
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+       
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ]
+
+
+
+}
